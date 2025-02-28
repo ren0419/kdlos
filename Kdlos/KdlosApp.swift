0b476d8e17b4f11ae64d12fc9a2baf2e845c05c4
ren0419 <renrenren0419@gmail.com>
2025-02-28 10:55:39 +0900

    Initial Commit

---
+++ Kdlos/KdlosApp.swift
@@ -0,0 +1,17 @@
+//
+//  KdlosApp.swift
+//  Kdlos
+//
+//  Created by katahira ren on 2025/02/28.
+//
+
+import SwiftUI
+
+@main
+struct KdlosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
